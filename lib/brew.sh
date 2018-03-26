ce3eb929cf9de6e6be577b13de9cbac2fbc328f4
Deepak Kannan <kannan.deepak@gmail.com>
2018-03-27 01:45:21 +0530

    :bug: Fix bug where brew bundle cleanup would uninstall fira font

--- lib/brew.sh
+++ lib/brew.sh
@@ -14,14 +14,14 @@ fi
 
 # TODO: run periodically or fail fast if internet roundtrip is slow
 # brew update --force # https://github.com/Homebrew/brew/issues/1151
+cat "./configs/Brewfile" | brew bundle --file=-
+cat "./configs/Brewfile" | brew bundle cleanup --force --file=-
 
-# BUG: not in Brewfile as `brew bundle cleanup` cleans up the fonts!
+# BUG: not in Brewfile as `brew bundle cleanup` cleans up the fonts, even if Brewfile has the font tap and cask!
 brew tap caskroom/fonts
 brew cask install font-fira-code
 # system_profiler SPFontsDataType | grep 'FiraCode'
 
-cat "./configs/Brewfile" | brew bundle --file=-
-cat "./configs/Brewfile" | brew bundle cleanup --force --file=-
 brew cleanup
 # du -hc  `brew --cache`
 # brew doctor
