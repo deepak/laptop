e57c556a6e3c2b73907b12ae79c19b6683f70503
Deepak Kannan <kannan.deepak@gmail.com>
2018-04-03 14:22:41 +0530

    :bug: Fix bug where Brewfile cleanup was uninstalling font

--- lib/brew.sh
+++ lib/brew.sh
@@ -16,16 +16,16 @@ fi
 # NOTE: using `--force` due to https://github.com/Homebrew/brew/issues/1151
 # brew update --force
 
+# NOTE: `brew bundle` might fail,
+# can either run it repeatedly or install the individual formula/cask.
 cat "./configs/Brewfile" | brew bundle --file=-
 cat "./configs/Brewfile" | brew bundle cleanup --force --file=-
-
-# BUG: not in Brewfile as `brew bundle cleanup` cleans up the fonts, even if Brewfile has the font tap and cask!
-brew tap caskroom/fonts
-brew cask install font-fira-code
-# system_profiler SPFontsDataType | grep 'FiraCode'
 
 brew cleanup
 # du -hc  `brew --cache`
 # brew doctor
+# system_profiler SPFontsDataType | grep 'FiraCode'
+
+# check `brew leaves` for leaf nodes of the dependency graph. can uninstall if not used
 
 echo "[INFO] install xcode command line tools and homebrew"
