9caffa01194752ba0eaac6f36a0ba295f8392410
Deepak Kannan <kannan.deepak@gmail.com>
2018-04-29 11:15:14 +0530

    :sparkles: Install github-linguist and add dns prefetch for google fonts

--- lib/ruby.sh
+++ lib/ruby.sh
@@ -11,11 +11,12 @@ cat > "$HOME/.default-gems" <<EOF
 bundler
 pry
 overcommit
+github-linguist
 EOF
 
 asdf install ruby 2.5.1
 asdf global ruby 2.5.1
 
-gem install bundler pry overcommit &> /dev/null
+gem install bundler pry overcommit github-linguist &> /dev/null
 
 echo "[INFO] install ruby and gems"
